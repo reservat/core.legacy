7de0af3d3de0f423702d9de62026444d550e75b7
Paul Westerdale <paul@westerdale.me>
2015-04-18 16:19:03 +0100

    add updates to entity class

--- src/Core/Entity.php
+++ src/Core/Entity.php
@@ -7,10 +7,9 @@ abstract class Entity
 
     private $id = null;
 
-    public static function create($data)
+    public static function create($data, $repo)
     {
         $entity = new static();
-        $repo = 'Reservat\Repository\\' . (new \ReflectionClass($entity))->getShortName() . 'Repository';
 
         foreach ($data as $key => $value) {
             $isFillable = in_array($key, array_keys($repo::$fillable));
