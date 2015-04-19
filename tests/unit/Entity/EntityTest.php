fdb0733d0a6bb07a5976b533dcff4e01355bc4f2
Paul Westerdale <paul@westerdale.me>
2015-04-19 11:05:43 +0100

    add DI to tests

--- tests/unit/Entity/EntityTest.php
+++ tests/unit/Entity/EntityTest.php
@@ -4,15 +4,23 @@ namespace Reservat\Test;
 
 use \Reservat\Core\Config;
 use \Reservat\Repository\BarRepository;
+use Aura\Di\Container;
+use Aura\Di\Factory;
 
 class EntityTest extends \PHPUnit_Framework_TestCase
 {
     protected $bar = null;
+    protected $di = null;
 
     public function setUp()
     {
         require_once(__DIR__.'/_files/bar.php');
         require_once(__DIR__.'/_files/BarRepository.php');
+
+        $this->di = new Container(new Factory);
+        $this->di->set('db', function () {
+            return new \PDO('sqlite::memory:');
+        });
     }
 
     public function testCreate()
@@ -20,7 +28,7 @@ class EntityTest extends \PHPUnit_Framework_TestCase
         $bar = \Reservat\Bar::create([
             'name' => 'Paul Westerdale',
             'email' => 'paul@westerdale.me'
-        ], new BarRepository());
+        ], new BarRepository($this->di->get('db')));
 
         $this->assertEquals($bar->getName(), 'Paul Westerdale');
         $this->assertEquals($bar->getEmail(), 'paul@westerdale.me');
@@ -34,7 +42,7 @@ class EntityTest extends \PHPUnit_Framework_TestCase
             'name' => 'Paul Westerdale',
             'email' => 'paul@westerdale.me',
             'shoesize' => 21
-        ], new BarRepository());
+        ], new BarRepository($this->di->get('db')));
         
     }
 }
